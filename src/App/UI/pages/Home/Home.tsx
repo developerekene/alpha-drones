8ede57dc74998b64c9f52b7ab25efae471d53bcb
developerekene <developerekene@gmail.com>
2023-05-14 19:24:39 +0100

    first commit

---
+++ src/App/UI/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button, colors } from "@mui/material";
+
+export const windowWidth: number = window.screen.width;
+export const windowHeight: number = window.screen.height;
+
+const Home: React.FC<any> = ({}) => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default Home;
