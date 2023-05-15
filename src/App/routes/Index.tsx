14df3a61972e00f6f2a1cbfaeda6a0c98cf910d4
terryEda <okwarieda@gmail.com>
2023-05-15 20:28:11 +0100

    eda branch created

--- src/App/routes/Index.tsx
+++ src/App/routes/Index.tsx
@@ -3,11 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../UI/pages/Home/Home";
 import Contact from "../UI/pages/Contact/Contact";
 import About from "../UI/pages/About/About";
-import SoftWareEng from "../UI/pages/SoftWareEng/SoftWareEng";
-import Drone from "../UI/pages/Drone/Drone";
-import ELearning from "../UI/pages/E-Learning/E-Learning";
-import Mobile from "../UI/pages/Mobile/Mobile";
-import AniMax from "../UI/pages/AniMax/AniMax";
 import Portfolio from "../UI/pages/Portfolio/Portfolio";
 
 const Index: React.FC = () => {
@@ -17,12 +12,7 @@ const Index: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/softwareeng" element={<SoftWareEng />} />
-        <Route path="/drone" element={<Drone/>} />
-        <Route path="/elearning" element={<ELearning/>} />
-        <Route path="/mobile" element={<Mobile/>} />
-        <Route path="/animax" element={<AniMax/>} />
-        <Route path="/portfolio" element={<Portfolio/>}/>
+        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
     </BrowserRouter>
   );
