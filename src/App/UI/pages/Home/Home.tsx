2b7e5c98b7fcbded97dd49467c1e73620953e28b
developerekene <developerekene@gmail.com>
2023-05-20 19:45:04 +0100

    Home page almost done

--- src/App/UI/pages/Home/Home.tsx
+++ src/App/UI/pages/Home/Home.tsx
@@ -27,8 +27,78 @@ const Home: React.FC<any> = ({}) => {
         }
       />
       <div>
-        <p>khfsugiusgdcusg</p>
-        <i className="fa-sharp fa-regular fa-drone"></i>
+        <div className="middleContainerDiv">
+          <h2 className="middleHeading">Say Hi to</h2>
+          <h2 className="middleParagraph">DJI Mini 2 SE</h2>
+        </div>
+        <div className="droneImageDiv">
+          <img
+            src={`${Assets.images.homeMainDrone}`}
+            width={"100%"}
+            height={500}
+          />
+        </div>
+        <div className="titleDiv">
+          <div>
+            <img src={`${Assets.images.thermalCamera}`} />
+            <h3 className="headings">Thermal Camera</h3>
+            <p className="paragraph">
+              A thermal imaging camera is a type of the thermographic camera
+              used in firefighting
+            </p>
+          </div>
+          <div>
+            <img src={`${Assets.images.roboticArm}`} />
+            <h3 className="headings">Robotic platform</h3>
+            <p className="paragraph">
+              ROP is aims to make hardware designs of robots available under an
+              Open Hardware license .
+            </p>
+          </div>
+          <div>
+            <img src={`${Assets.images.mpCamera}`} />
+            <h3 className="headings">48 MP Camera</h3>
+            <p className="paragraph">
+              For most photos, the quad-pixel sensor combines every four pixels
+              into one large quad pixel..
+            </p>
+          </div>
+          <div>
+            <img src={`${Assets.images.fiveG}`} />
+            <h3 className="headings">5G Internet Connection</h3>
+            <p className="paragraph">
+              It is a new global wireless standard after 1G, 2G, 3G, and 4G
+              networks
+            </p>
+          </div>
+        </div>
+        <div className="buttonMiddle">
+          <a href="#">
+            <button className="buttonMainOrder">Order a Personal Drone</button>
+          </a>
+        </div>
+        <div className="contactDiv">
+          <div>
+            <img src={`${Assets.images.contact}`} />
+          </div>
+          <div className="contactForm">
+            <h2 className="contactDivP">
+              TELL US ABOUT YOUR INTERESTS AND WE WILL BE HAPPY TO GET BACK TO
+              YOU SHORTLY.
+            </h2>
+
+            <input className="inputHome" placeholder="Full Name" />
+            <input className="inputHome" placeholder="Email" />
+            <input className="inputHome" placeholder="Title" />
+            <input className="inputHome" placeholder="Message" />
+
+            <a href="#">
+              <button className="buttonMainContact">
+                Or send us a WhatsApp message
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/App/utils/Assets.ts
+++ src/App/utils/Assets.ts
@@ -1,5 +1,11 @@
 export const Assets = {
     images: {
         homeBackground: require("../images/png/home1.png"),
+        homeMainDrone: require('../images/png/droneHome.png'),
+        thermalCamera: require("../images/png/captureIcon.png"),
+        roboticArm: require("../images/png/robotIcon.png"),
+        mpCamera: require("../images/png/camera.png"),
+        fiveG: require("../images/png/fiveG.png"),
+        contact: require("../images/png/contact-removebg-preview.png")
     }
 }
