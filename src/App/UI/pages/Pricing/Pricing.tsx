5c1ea1f17802985e7d916db80a3d3bc27203110e
terryEda <okwarieda@gmail.com>
2023-07-07 18:29:12 +0100

    update

--- src/App/UI/pages/Pricing/Pricing.tsx
+++ src/App/UI/pages/Pricing/Pricing.tsx
@@ -1,10 +1,118 @@
 import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import MainContainer from "../../components/MainContainer/MainContainer";
+import { Assets } from "../../../utils/Assets";
+import "../Pricing/Pricing.css";
 
 const Pricing: React.FC<any> = () => {
   return (
-    <div>
-      <p>Pricing</p>
-    </div>
+    <main className="main">
+      <MainContainer
+        navView={<NavBar />}
+        backgroundImage={`${Assets.images.contactBackground}`}
+      />
+      <section className="section">
+        <div className="box-1">
+          <div className="u-box">
+            <p>Free Account</p>
+            <p>$300 / per month</p>
+          </div>
+          <div className="u-box">
+            <img src={`${Assets.images.solidcam}`} width={"25%"} height={40} />
+            <p>1 free credit </p>
+          </div>
+
+          <div className="u-box">
+            <img src={`${Assets.images.gis}`} width={"25%"} height={60} />
+            <p>Apps for Windows /</p>
+          </div>
+
+          <div className="u-box">
+            <img src={`${Assets.images.speaker}`} width={"25%"} height={60} />
+            <p>Refer friends and earn credits</p>
+
+            <button className="buttn" type="button">
+              Subscribe
+            </button>
+          </div>
+        </div>
+        <div className="box-2">
+          <p>Subscription plan</p>
+          <p>$500 / per month</p>
+          <ul className="ul">
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+          </ul>
+
+          <div className=" on">
+            <p>Pay monthly</p>
+            <img src={`${Assets.images.mdi}`} width={"10%"} height={30} />
+            <p>Pay yearly save 10%</p>
+          </div>
+          <div className="u-box">
+            <button className="buttn">Subscribe</button>
+          </div>
+        </div>
+        <div className="box-3">
+          <p>$400 / per month</p>
+          <ul className="ul-2">
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+            <li> 40 credits / month $ 9 $ 0.23 / image</li>
+          </ul>
+          <div className="u-box">
+            <button className="buttn">Buy Now</button>
+          </div>
+        </div>
+      </section>
+      <div>
+        <p className="fre">
+          <b>Frequently asked questions</b>
+        </p>
+        <table className="table">
+          <tr>
+            <td className="td">
+              Contrary to popular belief, Lorem Ipsum is not simply random text
+            </td>
+          </tr>
+          <tr>
+            <td className="td">
+              Contrary to popular belief, Lorem Ipsum is not simply random text
+            </td>
+          </tr>
+          <tr>
+            <td className="td">
+              Contrary to popular belief, Lorem Ipsum is not simply random text
+            </td>
+          </tr>
+          <tr>
+            <td className="td">
+              Contrary to popular belief, Lorem Ipsum is not simply random text
+            </td>
+          </tr>
+          <tr>
+            <td className="td">
+              Contrary to popular belief, Lorem Ipsum is not simply random text
+            </td>
+          </tr>
+        </table>
+        <p className="fre">
+          <b>
+            Trusted by 28,000+ customers from 200+ countries around the globe
+          </b>
+        </p>
+      </div>
+      <div className="logo">
+        <img src={`${Assets.images.logoA}`} width={"10%"} height={30} />
+        <img src={`${Assets.images.logoB}`} width={"10%"} height={30} />
+        <img src={`${Assets.images.logoC}`} width={"10%"} height={30} />
+      </div>
+    </main>
   );
 };
 
--- src/App/UI/pages/Services/Services.tsx
+++ src/App/UI/pages/Services/Services.tsx
@@ -1,10 +1,125 @@
 import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import MainContainer from "../../components/MainContainer/MainContainer";
+import { Assets } from "../../../utils/Assets";
+import "../Services/Services.css";
 
 const Services: React.FC<any> = () => {
   return (
-    <div>
-      <p>Services</p>
-    </div>
+    <main>
+      <MainContainer
+        navView={<NavBar />}
+        backgroundImage={`${Assets.images.contactHome}`}
+      />
+      <section className="sec">
+        <div>
+          <p className="trans">
+            <b>Transportation</b>
+          </p>
+          <p className="note">
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+        </div>
+        <div>
+          <p className="trans">
+            <b>Event coverage</b>
+          </p>
+          <p className="note">
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+        </div>
+        <div>
+          <p className="trans">
+            <b>Security</b>
+          </p>
+          <p className="note">
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+        </div>
+        <div>
+          <p className="trans">
+            <b>Video Coverage</b>
+          </p>
+          <p className="note">
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+        </div>
+      </section>
+      <article className="article">
+        <h4 className="h4">FEATURE DRONES</h4>
+        <p className="p-1">Make it Easy To in Control Drones</p>
+        <div className="feature">
+          <p>
+            You need a diam in the arc of the course of Euismod, who is the
+            <br />
+            cartoonist. Lake cartoon life homework football recipe and recipe
+            <br />
+            until and. Urn employee at the element of easy football. There is
+            <br />
+            always no bed at the end of life. Neither of life nor of children
+            nor <br />
+            of any kind. Start until the mass is correct The airline needs to be
+            <br />
+            smart The old man and the grandson were waiting
+          </p>
+          <div className="view">
+            <div className=" blue">
+              <img src={`${Assets.images.serve}`} width={"80%"} height={125} />
+            </div>
+            <div className="shape">
+              <p className="last-p">
+                Get cashback up 25% On your first <br /> purchase order Now,
+                let's get ready for life,  if you don't have a good life.
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </main>
   );
 };
 
